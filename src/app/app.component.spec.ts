eff9fef4059fa8311a0566ac980b8564208f7905
jabranemohamed <jabranemohamed@gmail.com>
2023-09-18 12:43:39 +0200

    add files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'securecapitaapp'`, () => {
+  it(`should have as title 'crmapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('securecapitaapp');
+    expect(app.title).toEqual('crmapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('securecapitaapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('crmapp app is running!');
   });
 });
